589c1bfccb9f25456faab21d2abf6c1d83b5b340
Maksim Bondaruk <mbondaruk@yandex.ru>
2024-02-20 22:59:51 +0500

    add lambda operations

--- Lambda/PlanetList.cs
+++ Lambda/PlanetList.cs
@@ -4,10 +4,10 @@ public class PlanetList
     static Planet venus = new Planet("Венера", 2, 38025, null);
     static Planet earth = new Planet("Земля", 3, 40075, venus.PlanetName);
     static Planet mars = new Planet("Марс", 4, 21344, earth.PlanetName);
-    private int faultQty = 0;
+    internal int faultQty = 0;
     public delegate string? PlanetValidator(string planetName);
-    
-    static List<Planet> innerSunOrbitList = new List<Planet>();
+
+    internal static List<Planet> innerSunOrbitList = new List<Planet>();
     public PlanetList()
     {
         innerSunOrbitList.Add(venus);
@@ -29,8 +29,14 @@ public class PlanetList
         }
         return (null, null, planetCheckFuncResultString);
     }
-    public string? PlanetChecker(string planetName)
+    /*public string? PlanetChecker(string planetName)
     {
+        faultQty++;
+        if (faultQty > 2)
+        {
+            faultQty = 0;
+            return ("Вы спрашиваете слишком часто");
+        }
         foreach (var planetCounter in innerSunOrbitList)
         {
             if (planetCounter.PlanetName == planetName)
@@ -38,14 +44,9 @@ public class PlanetList
                 return null;
             }
         }
-        faultQty++;
-        if (faultQty > 2)
-        {
-            faultQty = 0;
-            return ("Вы спрашиваете слишком часто");
-        }
         return ("Не удалось найти планету " + planetName);
     }
+    */
     public string? NameOfIndex(int? ordNum)
     {
         if (ordNum != null)
--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -1,25 +1,85 @@
 ﻿using Lambda;
 PlanetList myList = new PlanetList();
-var tuple = myList.GetPlanet("Земля", myList.PlanetChecker);
+
+PlanetList.PlanetValidator checkerLambda = (string planetName) =>
+{
+    myList.faultQty++;
+    if (myList.faultQty > 2)
+    {
+        myList.faultQty = 0;
+        return ("Вы спрашиваете слишком часто");
+    }
+    foreach (var planetCounter in PlanetList.innerSunOrbitList)
+    {
+        if (planetCounter.PlanetName == planetName)
+        {
+            return null;
+        }
+    }
+    return ("Не удалось найти планету " + planetName);
+};
+
+PlanetList.PlanetValidator checkerLambdaforLemony = (string planetName) =>
+{
+    //foreach (var planetCounter in PlanetList.innerSunOrbitList)
+    //{
+        if (planetName != "Лимония")
+        {
+            return null;
+        }
+    //}
+    return ("Это запретная планета");
+};
+
+//var tuple = myList.GetPlanet("Земля", myList.PlanetChecker);
+var tuple = myList.GetPlanet("Земля", checkerLambda);
 Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " +  tuple.ordNum + ", " +  tuple.equatorLen));
 
-tuple = myList.GetPlanet("Земля", myList.PlanetChecker);
+tuple = myList.GetPlanet("Земля", checkerLambda);
 Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " +  tuple.ordNum + ", " +  tuple.equatorLen));
 
-tuple = myList.GetPlanet("Лимония", myList.PlanetChecker);
+tuple = myList.GetPlanet("Лимония", checkerLambda);
 Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " +  tuple.ordNum + ", " +  tuple.equatorLen));
 
-tuple = myList.GetPlanet("Лимония", myList.PlanetChecker);
+tuple = myList.GetPlanet("Лимония", checkerLambda);
 Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " +  tuple.ordNum + ", " +  tuple.equatorLen));
 
-tuple = myList.GetPlanet("Венерия", myList.PlanetChecker);
+tuple = myList.GetPlanet("Венерия", checkerLambda);
 Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " +  tuple.ordNum + ", " +  tuple.equatorLen));
 
-tuple = myList.GetPlanet("Планета 17", myList.PlanetChecker);
+tuple = myList.GetPlanet("Планета 17", checkerLambda);
 Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " +  tuple.ordNum + ", " +  tuple.equatorLen));
 
-tuple = myList.GetPlanet("Марс", myList.PlanetChecker);
+tuple = myList.GetPlanet("Марс", checkerLambda);
 Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " +  tuple.ordNum + ", " +  tuple.equatorLen));
 
-tuple = myList.GetPlanet("Венера", myList.PlanetChecker);
+tuple = myList.GetPlanet("Венера", checkerLambda);
 Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " +  tuple.ordNum + ", " +  tuple.equatorLen));
+
+Console.WriteLine();
+Console.WriteLine("Тест на Лимонию");
+Console.WriteLine();
+
+tuple = myList.GetPlanet("Земля", checkerLambdaforLemony);
+Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " + tuple.ordNum + ", " + tuple.equatorLen));
+
+tuple = myList.GetPlanet("Земля", checkerLambdaforLemony);
+Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " + tuple.ordNum + ", " + tuple.equatorLen));
+
+tuple = myList.GetPlanet("Лимония", checkerLambdaforLemony);
+Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " + tuple.ordNum + ", " + tuple.equatorLen));
+
+tuple = myList.GetPlanet("Лимония", checkerLambdaforLemony);
+Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " + tuple.ordNum + ", " + tuple.equatorLen));
+
+tuple = myList.GetPlanet("Венерия", checkerLambdaforLemony);
+Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " + tuple.ordNum + ", " + tuple.equatorLen));
+
+tuple = myList.GetPlanet("Планета 17", checkerLambdaforLemony);
+Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " + tuple.ordNum + ", " + tuple.equatorLen));
+
+tuple = myList.GetPlanet("Марс", checkerLambdaforLemony);
+Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " + tuple.ordNum + ", " + tuple.equatorLen));
+
+tuple = myList.GetPlanet("Венера", checkerLambdaforLemony);
+Console.WriteLine((tuple.ordNum == null) ? tuple.fault : (myList.NameOfIndex(tuple.ordNum) + ", " + tuple.ordNum + ", " + tuple.equatorLen));
